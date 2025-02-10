1229a7967f4dcc41f3326fe180cea9d593632390
Cesar <chpvhr@gmail.com>
2025-02-10 20:46:13 -0300

    4. Formularios
    Se crean los componentes de creación y edición de géneros. Se crea componente reutilizable formulario-genero.

--- angular-peliculas/src/app/generos/crear-generos/crear-generos.component.ts
+++ angular-peliculas/src/app/generos/crear-generos/crear-generos.component.ts
@@ -1,20 +1,28 @@
 import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Router, RouterLink } from '@angular/router';
+import { primeraLetraMayuscula } from '../../compartidos/funciones/validaciones';
+import { FormularioGeneroComponent } from '../formulario-genero/formulario-genero.component';
+import { GeneroCreacionDTO } from '../generos';
 
 @Component({
   selector: 'app-crear-generos',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [FormularioGeneroComponent],
   templateUrl: './crear-generos.component.html',
   styleUrl: './crear-generos.component.css'
 })
 export class CrearGenerosComponent {
 
   private router = inject(Router);
-  
-  guardarCambios() {
+
+  guardarCambios(genero: GeneroCreacionDTO) {
     // ... guardar los cambios
-    this.router.navigate(['/generos']);
+    // this.router.navigate(['/generos']);
+
+    console.log('creando el género ', genero);
   }
 }
